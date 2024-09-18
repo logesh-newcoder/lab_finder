15af0a33c040e16d3065671df887149e9bbbd55b
logesh-newcoder <logeshakn45@gmail.com>
2024-09-18 20:01:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,128 @@
+var a=document.getElementById("show");
+
+function arowa(){
+    a.value="class-a"
+    console.log(a.value)
+}
+function arowb(){
+    a.value="class-b"
+    console.log(a.value)
+}
+function arowc(){
+    a.value="class-c"
+    console.log(a.value)
+}
+function arowd(){
+    a.value="class-d"
+    console.log(a.value)
+}
+function arowe(){
+    a.value="class-e"
+    console.log(a.value)
+}
+
+function browa(){
+    a.value="class-a"
+    console.log(a.value)
+}
+function browb(){
+    a.value="class-b"
+    console.log(a.value)
+}
+function browc(){
+    a.value="class-c"
+    console.log(a.value)
+}
+function browd(){
+    a.value="class-d"
+    console.log(a.value)
+}
+function browe(){
+    a.value="class-e"
+    console.log(a.value)
+}
+
+function crowa(){
+    a.value="class-a"
+    console.log(a.value)
+}
+function crowb(){
+    a.value="class-b"
+    console.log(a.value)
+}
+function crowc(){
+    a.value="class-c"
+    console.log(a.value)
+}
+function crowd(){
+    a.value="class-d"
+    console.log(a.value)
+}
+function crowe(){
+    a.value="class-e"
+    console.log(a.value)
+}
+
+function drowa(){
+    a.value="class-a"
+    console.log(a.value)
+}
+function drowb(){
+    a.value="class-b"
+    console.log(a.value)
+}
+function drowc(){
+    a.value="class-c"
+    console.log(a.value)
+}
+function drowd(){
+    a.value="class-d"
+    console.log(a.value)
+}
+function drowe(){
+    a.value="class-e"
+    console.log(a.value)
+}
+
+function erowa(){
+    a.value="class-a"
+    console.log(a.value)
+}
+function erowb(){
+    a.value="class-b"
+    console.log(a.value)
+}
+function erowc(){
+    a.value="class-c"
+    console.log(a.value)
+}
+function erowd(){
+    a.value="class-d"
+    console.log(a.value)
+}
+function erowe(){
+    a.value="class-e"
+    console.log(a.value)
+}
+
+function frowa(){
+    a.value="class-a"
+    console.log(a.value)
+}
+function frowb(){
+    a.value="class-b"
+    console.log(a.value)
+}
+function frowc(){
+    a.value="class-c"
+    console.log(a.value)
+}
+function frowd(){
+    a.value="class-d"
+    console.log(a.value)
+}
+function frowe(){
+    a.value="class-e"
+    console.log(a.value)
+}
+
