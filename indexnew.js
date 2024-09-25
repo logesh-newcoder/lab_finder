ebf3d22feee45e4df001813743fc6a6e5046cde0
logesh-newcoder <logeshakn45@gmail.com>
2024-09-25 11:32:27 +0530

    Update indexnew.js

--- indexnew.js
+++ indexnew.js
@@ -1,55 +1,167 @@
 var a = document.getElementById("show");
 var deleteButton = document.getElementById("right");
-deleteButton.style.display = "none"; 
+deleteButton.style.display = "none";
 
-var rows = {
-    a: ["arowa", "arowb", "arowc", "arowd", "arowe"],
-    b: ["browa", "browb", "browc", "browd", "browe"],
-    c: ["crowa", "crowb", "crowc", "crowd", "crowe"],
-    d: ["drowa", "drowb", "drowc", "drowd", "drowe"],
-    e: ["erowa", "erowb", "erowc", "erowd", "erowe"],
-    f: ["frowa", "frowb", "frowc", "frowd", "frowe"]
-};
+var selectedElement = null;
 
-function deliver(content) {
-    a.value = content.textContent; 
-    deleteButton.style.display = "block";
+function arowa() {
+    updateInputValue('arowa');
+}
+function arowb() {
+    updateInputValue('arowb');
+}
+function arowc() {
+    updateInputValue('arowc');
+}
+function arowd() {
+    updateInputValue('arowd');
+}
+function arowe() {
+    updateInputValue('arowe');
 }
 
-function deleteContent() {
-    a.value = ""; 
-    deleteButton.style.display = "none"; 
-    checkPassword();
+function browa() {
+    updateInputValue('browa');
+}
+function browb() {
+    updateInputValue('browb');
+}
+function browc() {
+    updateInputValue('browc');
+}
+function browd() {
+    updateInputValue('browd');
+}
+function browe() {
+    updateInputValue('browe');
+}
+
+function crowa() {
+    updateInputValue('crowa');
+}
+function crowb() {
+    updateInputValue('crowb');
+}
+function crowc() {
+    updateInputValue('crowc');
+}
+function crowd() {
+    updateInputValue('crowd');
+}
+function crowe() {
+    updateInputValue('crowe');
+}
+
+function drowa() {
+    updateInputValue('drowa');;
+}
+function drowb() {
+    updateInputValue('drowd');
+}
+function drowc() {
+    updateInputValue('drowc');
+}
+function drowd() {
+    updateInputValue('drowd');
+}
+function drowe() {
+    updateInputValue('drowe');
+}
+
+function erowa() {
+    updateInputValue('erowa');
+}
+function erowb() {
+    updateInputValue('erowb');
+}
+function erowc() {
+    updateInputValue('erowc');
+}
+function erowd() {
+    updateInputValue('erowd');
+}
+function erowe() {
+    updateInputValue('erowe');
+}
+
+function frowa() {
+    updateInputValue('frowa');
+}
+function frowb() {
+    updateInputValue('frowb');
+}
+function frowc() {
+    updateInputValue('frowc');
+}
+function frowd() {
+    updateInputValue('frowd');
+}
+function frowe() {
+    updateInputValue('frowe');
+}
+
+function deliver(content) {
+    a.value = content.textContent;
+    deleteButton.style.display = "block";
+    selectedElement = content;
+}
+
+function dele() {
+    a.value = "";
+    deleteButton.style.display = "none";
+    selectedElement = null;
 }
 
 function checkPassword() {
     var val = prompt("Please enter the password:");
-    var pas = "edit";    
+    var pas = "edit";
     if (val === pas) {
         console.log("edit");
-        var text=prompt("");
-        a.value=text;
-        localStorage.setItem("content",a.innerText)
+        var newText = prompt("Enter new text:");
+        a.value = newText;
+
+        if (selectedElement) {
+            selectedElement.textContent = newText;
+        }
+        
+        localStorage.setItem("a", newText);
     } else {
         alert("Password is wrong");
     }
 }
 
-function createRowFunctions(row) {
-    rows[row].forEach(id => {
-        window[id] = function() {
-            deliver(document.getElementById(id));
-        };
-    });
-}
-
-for (let row in rows) {
-    createRowFunctions(row);
-}
-
 deleteButton.addEventListener("click", function(event) {
-    event.preventDefault(); 
-    deleteContent();
+    event.preventDefault();
+    dele();
 });
 
-a.addEventListener("dblclick", checkPassword); 
+window.onload = function() {
+    const storedContent = localStorage.getItem("a");
+    if (storedContent) {
+        a.value = storedContent;
+        deleteButton.style.display = "block";
+    }
+};
+
+
+let currentSelectedElement = null;
+
+function updateInputValue(elementId) {
+    currentSelectedElement = document.getElementById(elementId);
+    document.getElementById('show').value = currentSelectedElement.innerText;
+}
+
+function updateClassContent() {
+    if (currentSelectedElement) {
+        currentSelectedElement.innerText = document.getElementById('show').value;
+    }
+}
+
+document.getElementById('show').addEventListener('change', updateClassContent);
+
+document.getElementById('show').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        updateClassContent();
+        event.preventDefault();
+    }
+});
